2f2ac1e910111880c363be6bc06286ead692e787
belista <belista666@outlook.com>
2020-10-20 14:51:42 +0300

    fixed lazy loading.

--- PWApplication/src/app/app-routing.module.ts
+++ PWApplication/src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { MainComponent } from './main/main.component';
 import { AuthGuard } from '../app/shared/guards/auth.guard';
 
 const routes: Routes = [
   { path: "", canActivate: [AuthGuard], loadChildren: () => import("./main/main.module").then(m => m.MainModule)},
   { path: "main", canActivate: [AuthGuard], loadChildren: () => import("./main/main.module").then(m => m.MainModule)},
-  { path: "login", component: LoginComponent},
+  { path: "login", loadChildren: () => import("./login/login.module").then(m => m.LoginModule)},
   { path: "**", canActivate: [AuthGuard], loadChildren: () => import("./main/main.module").then(m => m.MainModule)}
 ];
 
